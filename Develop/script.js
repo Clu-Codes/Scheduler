777d3634d3793707c1e702e69553d7d85a4c6e42
Clu <retsulcconnor@gmail.com>
2020-11-14 17:39:25 -0500

    fixed css issue so save icon changes when hovered over. Removed unnecessary console.logs

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,3 @@
-// Pseudocode
-// Current Day - I will use Moment.js (or looking into modern alternatives) to have the current date display
-// Time Blocks - The time blocks need to present the 9-5 work week. Let's create them via bootstrap rows and columns - we will want to use col-12 here with many different rows
-// Using jQuery and moment.js we will color-coordinate the timeblocks with the current time
-// add an input box to the largest columns, expand the input field to be the entirety of the column
-// Use jQuery with blur, onChange, and focus to add the ability to update tasks within the planner and have them save 
-// Use Iconic for icons & Google Fonts for the fonts.
-
 var tasks = [];
 
 // store the object properties in my array to localStorage under the key of localStorageTasks
@@ -19,7 +11,6 @@ var loadTask = function() {
     
     // iterate through each element, checking if the objData matches. If so, add the text in objTask to that element. 
     tasks.forEach((element, index) => {
-        console.log(element.objData);
         $(element.objData).text(element.objTask);
     })
 };
